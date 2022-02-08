4ff3c723708b55e9b81863aa1757e98498120b69
davelaltan@gmail.com <davelaltan@gmail.com>
2022-02-08 00:22:20 -0600

    pushing first changes for mp backend

---
+++ src/test/java/com/mpbackend/MpbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
